ee5260e6a3fe21fafe63b5ab2cba5a797798373f
miryam jungreis <miriyungrei@gmail.com>
2020-03-28 19:45:05 +0300

    test

--- FlightSimulatorApp/Model/MySimulatorCommunicator.cs
+++ FlightSimulatorApp/Model/MySimulatorCommunicator.cs
@@ -24,6 +24,7 @@ namespace FlightSimulatorApp.Model
             {
                 Console.WriteLine("EROR - failed to connect please try again");
                 // test
+                // test
             }
         }
         public void write(string command)
