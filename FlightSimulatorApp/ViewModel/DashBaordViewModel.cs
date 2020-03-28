5dde78800d6856c4bf26b2bd1743c39b793bea04
miryam jungreis <miriyungrei@gmail.com>
2020-03-28 19:30:43 +0300

    test

--- FlightSimulatorApp/ViewModel/DashBaordViewModel.cs
+++ FlightSimulatorApp/ViewModel/DashBaordViewModel.cs
@@ -6,13 +6,14 @@ using System.Text;
 using System.Threading.Tasks;
 using FlightSimulatorApp.Model;
 namespace FlightSimulatorApp.ViewModel
-{//b
+{
     class DashBaordViewModel : INotifyPropertyChanged
     {
         private IFlightSimulatorModel model;
 
         public event PropertyChangedEventHandler PropertyChanged;
-        public void NotifyPropertyChanged(string propName) { }
+
+
         public DashBaordViewModel(IFlightSimulatorModel model)
         {
             this.model = model;
@@ -22,11 +23,47 @@ namespace FlightSimulatorApp.ViewModel
             };
         }
 
-        
-       
-        public Double VM_Latitude
+        public void NotifyPropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
+
+        public double VM_Altitude
         {
             get { return model.Altitude; }
         }
+        public double VM_Roll
+        {
+            get { return model.Roll; }
+        }
+        public double VM_Pitch
+        {
+            get { return model.Pitch; }
+        }
+        public double VM_Altimeter
+        {
+            get { return model.Altimeter; }
+        }
+        public double VM_Heading
+        {
+            get { return model.Heading; }
+        }
+        public double VM_GroundSpeed
+        {
+            get { return model.GroundSpeed; }
+        }
+        public double VM_VerticalSpeed
+        {
+            get { return model.VerticalSpeed; }
+        }
+        public double VM_AirSpeed
+        {
+            get { return model.AirSpeed; }
+        }
+
     }
 }
