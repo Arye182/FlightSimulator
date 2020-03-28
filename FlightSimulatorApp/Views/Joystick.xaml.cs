b8b668ea0f1590e1339f2fb2cb363e413725dfa3
Arye Amsalem <arye.amsalem@gmail.com>
2020-03-28 23:09:42 +0300

    test

--- FlightSimulatorApp/Views/Joystick.xaml.cs
+++ FlightSimulatorApp/Views/Joystick.xaml.cs
@@ -7,6 +7,18 @@ using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows;
 using System.ComponentModel;
+using System.Windows.Media.Animation;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+
+//using FlightSimulator.Model.EventArgs;
+
 
 namespace FlightSimulatorApp.Views
 {
@@ -14,105 +26,92 @@ namespace FlightSimulatorApp.Views
     {
         // this methods are for the joystick
 
+        // data members
         private Point knob_point = new Point();
         private Point knob_first_point = new Point();
+        private double canvasWidth, canvasHeight;
+        private readonly Storyboard centerKnob;
 
-        private double elevatorValue ;
-        private double rudderValue ;
-
-        private double x;
-        private double y;
-
-        public double X_Joy
+        // properties
+        public static readonly DependencyProperty RudderProp = DependencyProperty.Register("Aileron", typeof(double), typeof(Joystick), null);
+        public static readonly DependencyProperty ElevatorProp = DependencyProperty.Register("Elevator", typeof(double), typeof(Joystick), null);
+        public double Rudder
         {
-            get { return this.x; }
-            set { this.x = value; }
+            get { return Convert.ToDouble(GetValue(RudderProp)); }
+            set { SetValue(RudderProp, value); }
         }
-        public double Y_Joy
+        public double Elevator
         {
-            get { return this.y; }
-            set { this.y = value; }
-        }
-        public double ElevatorValue
-        {
-            get
-            {
-                return (this.elevatorValue);
-            }
+            get { return Convert.ToDouble(GetValue(ElevatorProp)); }
             set
             {
-                this.elevatorValue = value;
-            }
-        }
-        public double RudderValue
-        {
-            get
-            {
-                return (this.rudderValue);
-            }
-            set
-            {
-                this.rudderValue = value;
+                SetValue(ElevatorProp, value);
             }
         }
 
+        // delegates
+        //public delegate void OnScreenJoystickEventHandler(Joystick sender, VirtualJoystickEventArgs args);
+        public delegate void EmptyJoystickEventHandler(Joystick sender);
+
+        // events
+        //public event OnScreenJoystickEventHandler Moved;
+        public event EmptyJoystickEventHandler Released;
+        public event EmptyJoystickEventHandler Captured;
+        public event PropertyChangedEventHandler PropertyChanged;
 
         // this methods are for the joystick
         public Joystick()
         {
             InitializeComponent();
+            Knob.MouseLeftButtonDown += Knob_MouseLeftButtonDown;
+            Knob.MouseLeftButtonUp += Knob_MouseLeftButtonUp;
+            Knob.MouseMove += Knob_MouseMove;
+            centerKnob = Knob.Resources["CenterKnob"] as Storyboard;
         }
-
         private void centerKnob_Completed(object sender, EventArgs e)
         {
-            //lalal
+            centerKnob.Stop();
         }
-
-        private void Knob_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
-        {
-            if(e.ChangedButton == MouseButton.Left)
-            {
-                knob_point = e.GetPosition(this);
-                knob_first_point = e.GetPosition(this);
-                Knob.CaptureMouse();
-            }
-        }
-
         private void Knob_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
         {
-            if (e.LeftButton == MouseButtonState.Pressed)
+            knob_point.X = e.GetPosition(this).X - knob_first_point.X;
+            knob_point.Y = e.GetPosition(this).Y - knob_first_point.Y;
+            if (Math.Sqrt(knob_point.X * knob_point.X + knob_point.Y * knob_point.Y) <= 67)
             {
-                knob_point.X = e.GetPosition(this).X - knob_first_point.X;
-                knob_point.Y = e.GetPosition(this).Y - knob_first_point.Y;
-                if (Math.Sqrt(knob_point.X * knob_point.X + knob_point.Y * knob_point.Y) <= 67)
-                {
-                    knobPosition.X = knob_point.X;
-                    knobPosition.Y = knob_point.Y;
-                    X_Joy = knobPosition.X;
-                    Y_Joy = knobPosition.Y;
-                    // convert the X,Y coordinates to range [-1,1]
-                    this.elevatorValue = (-(knobPosition.Y) / 65);
-                    //this.elevatorValueString = this.elevatorValue.ToString("0.00");
-                    this.rudderValue = (2 * (knobPosition.X + 65) / 130 - 1);
-                    RudderValue = this.rudderValue;
-                    ElevatorValue = this.elevatorValue;
-                    
-                   
-                }
-            }
-        }
+                knobPosition.X = knob_point.X;
+                knobPosition.Y = knob_point.Y;
+                // convert the X,Y coordinates to range [-1,1]
+                Rudder = (2 * (knobPosition.X + 65) / 130 - 1);
+                Elevator = (-(knobPosition.Y) / 65);
 
-        private void Knob_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+                Console.WriteLine(GetValue(ElevatorProp));
+                Console.WriteLine(GetValue(RudderProp));
+                Console.WriteLine(knobPosition.X);
+                Console.WriteLine(knobPosition.Y);
+            }
+/*            if (Moved == null)
+            {
+                return;
+            }*/
+        }
+        private void Knob_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            knob_point = e.GetPosition(this);
+            knob_first_point = e.GetPosition(this);
+            Captured?.Invoke(this);
+            Knob.CaptureMouse();
+        }
+        private void Knob_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
             knobPosition.X = 0;
             knobPosition.Y = 0;
-            this.elevatorValue = 0;
-            this.rudderValue = 0;
+            Elevator = 0;
+            Rudder = 0;
             Knob.ReleaseMouseCapture();
         }
-
-
-
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
     }
-
 }
