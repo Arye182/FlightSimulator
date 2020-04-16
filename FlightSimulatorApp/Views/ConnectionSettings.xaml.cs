565fa5051954efddf87d2cc94b913f024e96f83b
Arye Amsalem <arye.amsalem@gmail.com>
2020-04-16 19:52:15 +0300

    added buttons of connect in settings

--- FlightSimulatorApp/Views/ConnectionSettings.xaml.cs
+++ FlightSimulatorApp/Views/ConnectionSettings.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FlightSimulatorApp.Model;
+using FlightSimulatorApp.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,10 +22,27 @@ namespace FlightSimulatorApp.Views
     /// </summary>
     public partial class ConnectionSettings : Window
     {
+        readonly FlightSimulatorModel m = (Application.Current as App).Model;
+        readonly SettingsViewModel svm = (Application.Current as App).SVM;
         public ConnectionSettings()
         {
             DataContext = (Application.Current as App).SVM;
             InitializeComponent();
         }
+
+
+        private void SaveClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void SaveConnect_Click(object sender, RoutedEventArgs e)
+        {
+            m.connect(svm.VM_IP, svm.VM_Port);
+            m.start();
+            this.Close();
+        }
+
+
     }
 }
